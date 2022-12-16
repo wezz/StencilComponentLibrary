ecdb79dc26c6c3514714dca77a42995b1eccf7dc
Wezz Balk <wezz@wezz.se>
2022-12-16 14:09:04 +0100

    Disabling shadowdom

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -3,7 +3,7 @@ import { Component, Prop, h } from '@stencil/core';
 @Component({
   tag: 'my-component',
   styleUrl: 'my-component.scss',
-  shadow: true,
+  shadow: false,
 })
 export class MyComponent {
 
